2da9f08d0c2a33a47996b636d19ee683c6818f01
devarcher <archershot@hotmail.com>
2019-11-03 11:11:49 -0600

    added list creation section for errors. not functioning

--- public/javaScript/forms/reg/reg1.js
+++ public/javaScript/forms/reg/reg1.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form1");
+const form = document.getElementById("form");
 const errorTitle = document.getElementById('classReg')
 const errorElement = document.getElementById("error");
 
@@ -7,15 +7,32 @@ form.addEventListener("submit", e => {
   
   // Empty Fields Errors
   for (let i = 0; i < form.elements.length; i++) {
-    if (form[i].type === "text" && form[i].value === "") {
+    if (form[i].type === "text" && form[i].value === "" || form[i].value === null) {
       messages.push(`Please fill out ${form[i].name}`);
     }
+  }
+
+  // Create a list in <div>#error</div> of error messages 
+  for(let i = 0; i < messages; i++) {
+    const list = document.createElement('ul');
+    list.className = "list";
+    const listItem = document.createElement('li');
+    let listValue = document.createTextNode(messages[i]);
+
+    // Append value to list item
+    listItem.appendChild(listValue);
+
+    // Append list item into the list
+    list.appendChild(listItem)
+
+    // Append dynamic list to Error Section
+    errorElement.appendChild(list);
   }
 
   // If Error Stop Submit page, Display Errors
   if (messages.length > 0) {
     e.preventDefault();
     errorTitle.innerText = ('Class Registration - ERROR')
-    errorElement.innerText = messages.join(', ')
+    
   }
 });
