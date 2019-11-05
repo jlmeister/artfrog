3ef9058467946a28a5ac90c84795276dbc198a60
kjschwoerke <kjschwoerke@yahoo.com>
2019-11-05 15:23:58 -0600

    alternate class list colors

--- public/javaScript/classes.js
+++ public/javaScript/classes.js
@@ -1,5 +1,4 @@
 //Create the class object constructor for Courses.
-
 class ArtCourse {
     constructor(id, name, desc, img){
         this.id = id;
@@ -10,20 +9,28 @@ class ArtCourse {
 
 }
 
+//create an Array to hold courses being created.
 const classArry = []
-//const classStr = classArry[0].join()
 
-//create a function to create a new course and push to an array
+//create sample courses
   let newCourse = new ArtCourse ("1", "Beginner Painting", "This painting course is designed for beginners with little or no previous painting experience ", "img")
   classArry.push(newCourse)
 
   let newCourse1 = new ArtCourse ("2", "Beginner Drawing", "This drawing course is designed for beginners with little or no previous painting experience ", "img")
   classArry.push(newCourse)
 
+//create counters to identify programmatically created elements
 console.log(newCourse)
 let counterArt = 0
 let counterUl = 2
 
+//Create function to alternate class listing background color
+function articleColor(){
+    let articleBkGrnd = document.getElementsByTagName("article")[counterArt]
+    if(counterArt %2!==0){articleBkGrnd.style.backgroundColor = "#22313F"; articleBkGrnd.style.color = '#f0f0f0'} else {articleBkGrnd.style.backgroundColor ='#f0f0f0'; articleBkGrnd.style.color = "#22313F"}
+}
+
+//create a function to create a new course and push to an array
 function appendClassList(classid) {
     // let classNode = document.create(classid);
     let nodeContainer = document.createElement('ARTICLE')
@@ -43,11 +50,14 @@ function appendClassList(classid) {
     let nodeButton = document.createElement('button')
     nodeButton.innerHTML = `Register for ${classid.name}`
     nodeArticle.appendChild(nodeButton)
-
+    articleColor()
     counterUl+=1
     counterArt+=1
+    
 // node.appendChild(classNode);
 }
 
 appendClassList(newCourse)
 appendClassList(newCourse1)
+
+//Dynamically change Course listing background colors so that every other course entry has a different background color:
