b3b56db0d1a9f4cec976a8e42c65b622e6745bd0
devarcher <archershot@hotmail.com>
2019-11-08 10:35:30 -0600

    added phone logic. success

--- public/javaScript/forms/reg/reg1.js
+++ public/javaScript/forms/reg/reg1.js
@@ -122,19 +122,16 @@ let detectZipError = inputListField => {
 };
 
 let detectPhoneError = inputListField => {
-  let phoneInput = document.querySelectorAll("input[name=phone]");
-  // let studentPhoneInput = document.getElementById("studentPhone");
-  // console.log(studentPhoneInput.value)
+  let phoneInput = inputListField;
 
   const re = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
-  for (let i = 0; i < phoneInput.length; i++) {
-    let phoneValue = phoneInput[i].value;
-    console.log(phoneValue);
-    console.log(re.test(String(phoneValue)));
-    console.log(phoneInput);
-    if (inputListField.name === "phone" && inputListField.value !== '' && !re.test(String(phoneValue))) {
-      return true;
-    }
+
+  if (
+    inputListField.name === "phone" &&
+    inputListField.value !== "" &&
+    !re.test(String(phoneInput.value))
+  ) {
+    return true;
   }
 };
 
