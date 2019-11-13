6c04c8fa0811c014224d9f58e7b012d6621daffd
kjschwoerke <kjschwoerke@yahoo.com>
2019-11-12 20:03:41 -0600

    svg working

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const classesRouter = require('./routes/classes');
 
 const PORT = process.env.PORT || 80;
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'projectx1',
+    database: 'testdb'
+
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PW,
+  // database: process.env.DB
 });
 
 db.connect(err => {
