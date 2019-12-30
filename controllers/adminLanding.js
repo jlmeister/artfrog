b625b5d432d4218f8c06325bc71f523a2372653e
devarcher <archershot@hotmail.com>
2019-12-30 11:32:05 -0600

    single query promise + async/await functional for getLandingPage controller

--- controllers/adminLanding.js
+++ controllers/adminLanding.js
@@ -1,30 +1,40 @@
-const getLandingPage = async (req, res) => {
-  let classes = {};
-  let teachers = {};
+let classes = {};
+let teachers = {};
 
-  const sqlClasses = 'SELECT * FROM classes';
-  await db.query(sqlClasses, (err, classResults) => {
-    if (err) {
-      console.log('********** ERROR REQUESTING FROM DATABASE *************');
-      throw err;
-    }
-    console.log(classResults);
-    // res.json(results);
-    classes = { classes: classResults };
+const getClasses = () =>
+  new Promise((resolve, reject) => {
+    const sqlClasses = 'SELECT * FROM classes';
+    db.query(sqlClasses, (err, classResults) => {
+      if (err) {
+        console.log('********** ERROR REQUESTING FROM DATABASE *************');
+        throw err;
+      }
+      resolve((classes = { classes: classResults }));
+    });
   });
 
-  const sqlTeachers = 'SELECT * FROM teachers';
-  await db.query(sqlTeachers, (err, teacherResults) => {
-    if (err) {
-      console.log('********** ERROR REQUESTING FROM DATABASE *************');
-      throw err;
-    }
-    console.log(teacherResults);
-    // res.json(results);
-    teachers = { teachers: teacherResults };
+const getTeachers = () =>
+  new Promise((resolve, reject) => {
+    const sqlTeachers = 'SELECT * FROM teachers';
+    db.query(sqlTeachers, (err, tearcherResults) => {
+      if (err) {
+        console.log('********** ERROR REQUESTING FROM DATABASE *************');
+        throw err;
+      }
+      resolve((teachers = { teachers: tearcherResults }));
+    });
   });
 
-  res.render('adminLanding.ejs', classes, teachers);
-};
+async function getLandingPage(req, res) {
+  try {
+    await getClasses();
+    console.log('classes inside getLandingPage()', classes);
+    await getTeachers();
+    console.log('teachers inside getLandingPage()', teachers);
+    await res.render('adminLanding.ejs', classes);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 module.exports = { getLandingPage };
