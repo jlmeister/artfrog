cdccf34fcbb517ba79f0d3e725464bfce1c9f5f6
devarcher <archershot@hotmail.com>
2019-12-31 10:03:11 -0600

    populating .ejs with simple data

--- controllers/adminLanding.js
+++ controllers/adminLanding.js
@@ -22,15 +22,44 @@ const getTeachers = () =>
     });
   });
 
+const getStudents = () =>
+  new Promise((resolve, reject) => {
+    const sqlStudents = 'SELECT * FROM students';
+    db.query(sqlStudents, (err, studentResults) => {
+      if (err) {
+        console.log('********** ERROR REQUESTING FROM DATABASE *************');
+        reject(err);
+      }
+      resolve({ students: studentResults });
+    });
+  });
+
+const getBoardMembers = () =>
+  new Promise((resolve, reject) => {
+    const sqlBoard = 'SELECT * FROM board_members';
+    db.query(sqlBoard, (err, boardResults) => {
+      if (err) {
+        console.log('********** ERROR REQUESTING FROM DATABASE *************');
+        reject(err);
+      }
+      resolve({ board: boardResults });
+    });
+  });
+
 async function getLandingPage(req, res) {
   try {
-    const dataGroup = await Promise.all([getClasses(), getTeachers()]).then(
-      data => ({
-        classes: JSON.parse(JSON.stringify(data[0].classes)),
-        teachers: JSON.parse(JSON.stringify(data[1].teachers)),
-      })
-    );
-
+    const dataGroup = await Promise.all([
+      getClasses(),
+      getTeachers(),
+      getStudents(),
+      getBoardMembers(),
+    ]).then(data => ({
+      classes: JSON.parse(JSON.stringify(data[0].classes)),
+      teachers: JSON.parse(JSON.stringify(data[1].teachers)),
+      students: JSON.parse(JSON.stringify(data[2].students)),
+      board: JSON.parse(JSON.stringify(data[3].board)),
+    }));
+    console.log(dataGroup);
     await res.render('adminLanding.ejs', { dataGroup });
   } catch (err) {
     console.log(err);
