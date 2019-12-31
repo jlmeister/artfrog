bb7a9de56f7a6bcee8be24d7bd2025de43e73997
devarcher <archershot@hotmail.com>
2019-12-31 10:42:09 -0600

    classes basic styling in place

---
+++ public/javaScript/admin/adminLanding.js
@@ -0,0 +1,4 @@
+const infoLine = document.getElementsByClassName('infoLine');
+const infoText = document.getElementsByClassName('infoText');
+const buttonBox = document.getElementsByClassName('buttonBox');
+
