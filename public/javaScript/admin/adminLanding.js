9d6819ba2609007ebbccb2ed27a5ae5fe9f312cb
devarcher <archershot@hotmail.com>
2020-01-01 14:50:05 -0600

    removed moment CDN

--- public/javaScript/admin/adminLanding.js
+++
@@ -1,9 +0,0 @@
-const convertDate = () => {
-  let classDate = document.getElementById('classDate').innerHTML;
-  console.log(classDate);
-  const date = moment(classDate).format('MM-DD-YYYY');
-  console.log(date);
-  classDate = date;
-};
-
-convertDate();
