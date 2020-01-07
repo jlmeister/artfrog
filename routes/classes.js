e85793435d7f49338405e845a1aacbb921cd84d4
devarcher <archershot@hotmail.com>
2020-01-06 19:53:52 -0600

    changed adminaddclass to .ejs

--- routes/classes.js
+++ routes/classes.js
@@ -5,7 +5,7 @@ const classesController = require('../controllers/classes');
 
 router.get('/classes', classesController.getAllClasses);
 // router.get('/admin/classes/:id', classesController.showClass); // make this controller, add auth
-// router.post('/classes', classesController.addNewClass); // add auth
+router.post('/classes', classesController.addNewClass); // add auth
 // router.put('/classes/:id', classesController.editClass); // make this controller, add auth
 // router.delete('classes/:id', classesController.removeClass); // make this controller, add auth
 
