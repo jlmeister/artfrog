faa69379d513b20319709ffce8cd935a3ea410fe
devarcher <archershot@hotmail.com>
2020-02-11 20:54:45 -0600

    cleaned up student routes

--- routes/students.js
+++ routes/students.js
@@ -5,8 +5,8 @@ const studentsController = require('../controllers/students');
 
 router.get('/students', studentsController.getAllStudents);
 
-// router.get('/students/contact', studentsController.showStudent); // make controller, add auth
-// router.put('/students/:id', studentsController.editStudent); // make controller, add auth
-// router.delete('/students/:id', studentsController.removeStudent); // make controller, add auth
+router.put('/students', studentsController.editStudent);
+
+router.delete('/students', studentsController.deleteStudent)
 
 module.exports = router;
