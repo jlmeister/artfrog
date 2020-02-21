3f5a36be820a35b9f2414557f62b7e25dcba782d
devarcher <archershot@hotmail.com>
2020-02-21 13:50:33 -0600

    added swich statement in handle change. will need to adjust for empty amount field on submit

--- public/react/payments.js
+++ public/react/payments.js
@@ -1,29 +1,124 @@
 class CheckoutForm extends React.Component {
   state = {
-    name: '',
-    amount: '',
+    name: null,
+    amount: null,
     paymentSucceeded: false,
+    errors: {
+      name: '',
+      amount: '',
+    },
   };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    const { errors } = this.state;
+    // this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        errors.name =
+          value.length < 4 ? 'Full Name must be at least 4 characters' : '';
+        break;
+      case 'amount':
+        errors.amount = value.length !== 0 ? 'Amount must have value' : '';
+        break;
+      default:
+        break;
+    }
+    this.setState({ errors, [name]: value }, () => {
+      console.log(errors);
+    });
+  };
+
+  // errorTrue = () => {
+  //   this.state.name === '' && (
+  //     <React.Fragment>
+  //       <form onSubmit={this.handleSubmit}>
+  //         <label style={{ color: 'red' }}>* Full Name is Required: </label>
+  //         <br />
+  //         <input
+  //           id="name"
+  //           type="text"
+  //           value={this.state.name}
+  //           onChange={e => this.setState({ name: e.target.value })}
+  //           placeholder="Full Name"
+  //           style={{ outline: '3px solid red' }}
+  //         ></input>
+  //         <br />
+  //         <label>Amount: </label>
+  //         <br />
+  //         <input
+  //           id="amount"
+  //           type="number"
+  //           value={this.state.amount}
+  //           onChange={e => this.setState({ amount: e.target.value })}
+  //           placeholder="Amount"
+  //         ></input>
+  //         <br />
+  //         <label>
+  //           Card Details
+  //           <ReactStripeElements.CardElement />
+  //         </label>
+  //         <button>Donate</button>
+  //       </form>
+  //     </React.Fragment>
+  //   );
+  //   this.state.amount === '' && (
+  //     <React.Fragment>
+  //       <form onSubmit={this.handleSubmit}>
+  //         <label>Full Name: </label>
+  //         <br />
+  //         <input
+  //           id="name"
+  //           type="text"
+  //           value={this.state.name}
+  //           onChange={e => this.setState({ name: e.target.value })}
+  //           placeholder="Full Name"
+  //         ></input>
+  //         <br />
+  //         <label style={{ color: 'red' }}>Amount is Required: </label>
+  //         <br />
+  //         <input
+  //           id="amount"
+  //           type="number"
+  //           value={this.state.amount}
+  //           onChange={e => this.setState({ amount: e.target.value })}
+  //           placeholder="Amount"
+  //           style={{ outline: '3px solid red' }}
+  //         ></input>
+  //         <br />
+  //         <label>
+  //           Card Details
+  //           <ReactStripeElements.CardElement />
+  //         </label>
+  //         <button>Donate</button>
+  //       </form>
+  //     </React.Fragment>
+  //   );
+  // };
 
   handleSubmit = async e => {
     e.preventDefault();
-    try {
-      const { token } = await this.props.stripe.createToken({
-        name: this.state.name,
-      });
-      const { amount } = this.state;
-      await fetch('api/donate', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({ token, amount }),
-      }).then(res => {
-        this.paymentSuccess();
-      });
-      console.log(token);
-    } catch (err) {
-      throw err;
+    if (this.state.name && this.state.amount !== '') {
+      try {
+        const { token } = await this.props.stripe.createToken({
+          name: this.state.name,
+        });
+        const { amount } = this.state;
+        await fetch('api/donate', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({ token, amount }),
+        }).then(res => {
+          this.paymentSuccess();
+        });
+        console.log(token);
+      } catch (err) {
+        throw err;
+      }
     }
   };
 
@@ -32,7 +127,7 @@ class CheckoutForm extends React.Component {
   };
 
   render() {
-    console.log(this.state, this.props);
+    console.log(this.state);
     return this.state.paymentSucceeded ? (
       <p>Thank you for your Donation!</p>
     ) : (
@@ -40,22 +135,22 @@ class CheckoutForm extends React.Component {
         <label>Full Name: </label>
         <br />
         <input
-          required
           id="name"
+          name="name"
           type="text"
           value={this.state.name}
-          onChange={e => this.setState({ name: e.target.value })}
+          onChange={e => this.handleChange(e)}
           placeholder="Full Name"
         ></input>
         <br />
         <label>Amount: </label>
         <br />
         <input
-          required
           id="amount"
+          name="amount"
           type="number"
           value={this.state.amount}
-          onChange={e => this.setState({ amount: e.target.value })}
+          onChange={e => this.handleChange(e)}
           placeholder="Amount"
         ></input>
         <br />
